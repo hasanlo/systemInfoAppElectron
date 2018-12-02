d245b79e08067f3b73399b8c0582b22b5dd6f488
milad hasanlo <milad.hasanlo@gmail.com>
2018-12-02 21:42:15 +0330

    electron sample

---
+++ main.js
@@ -0,0 +1,29 @@
+const {app,BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+ 
+let win;
+
+function createWindow(){
+    win = new BrowserWindow({width:600 , height: 800,icon: __dirname+'/assets/img/system-settings-icon.png'});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname,'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win.webContents.openDevTools();
+
+    win.on('close',()=>{
+        win = null;
+    })
+}
+
+app.on('ready',createWindow); 
+
+app.on('window-all-closed',()=>{
+    if(process.platform != 'darwin'){
+        app.quit();
+    }
+})
